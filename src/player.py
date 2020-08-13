972e614b0e7971b869d8eaa51a8dd97840c84664
unknown <Dustin.Guillen14@yahoo.com>
2020-08-12 19:13:25 -0600

    player class/room class / set up adventure_game.py

---
+++ src/player.py
@@ -0,0 +1,4 @@
+#### ========================================= PLAYER =========================================
+class Player:
+    def __init__(self, current_room=None):
+        self.current_room = current_room
